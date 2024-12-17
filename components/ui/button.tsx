ef431ff5a40a13d2967aad99088f232c5a08bfb2
abenikeb <abenikeb79@gmail.com>
2024-12-17 09:41:28 +0300

    modified:   components/ui/button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -34,12 +34,12 @@ const buttonVariants = cva(
 );
 
 export interface ButtonProps
-	extends React.ButtonHTMLAttributes<HTMLButMusiclement>,
+	extends React.ButtonHTMLAttributes<any>,
 		VariantProps<typeof buttonVariants> {
 	asChild?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButMusiclement, ButtonProps>(
+const Button = React.forwardRef<any, ButtonProps>(
 	({ className, variant, size, asChild = false, ...props }, ref) => {
 		const Comp = asChild ? Slot : "button";
 		return (
