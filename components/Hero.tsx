6c387320ce8e7a06359fce9221d0ed9dd7068675
abenikeb <abenikeb79@gmail.com>
2024-12-17 13:53:18 +0300

    modified:   components/Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,18 +10,18 @@ export function Hero() {
 		<section className="relative py-32 overflow-hidden">
 			<div className="absolute inset-0 z-0 bg-[#238dcb]">
 				<img
-					src="/assets/images/promo5.png?height=1080&width=1920"
+					src="/assets/images/promo7.png?height=1080&width=1920"
 					alt="Background"
-					className="w-full h-full object-cover opacity-10"
+					className="w-full h-full object-contain opacity-20"
 				/>
 				<div className="absolute inset-0 bg-black opacity-30">j</div>
 			</div>
 			<div className="relative z-10 container mx-auto text-center text-white">
-				<h2 className="text-5xl font-bold mb-6 leading-tight">
-					Revolutionizing{" "}
-					<span className="text-lime-400">mobile advertising in Ethiopia.</span>
+				<h2 className="text-6xl font-extrabold mb-6 leading-tight">
+					Revolutionizing Mobile Advertising in{" "}
+					<span className="text-lime-400">Ethiopia.</span>
 				</h2>
-				<p className="text-xl mb-10 max-w-2xl mx-auto">
+				<p className="text-2xl mb-10 max-w-2xl mx-auto">
 					Elevate Your Brand with the Power of Ring Back Advertising!
 				</p>
 
